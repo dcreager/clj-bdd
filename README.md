d26f6bd2d0995c2304d800742c5be4429beac3a2
Douglas Creager <douglas.creager@redjack.com>
2014-06-13 09:26:44 -0400

    Initial clj-bdd project template

---
+++ README.md
@@ -0,0 +1,12 @@
+# clj-bdd
+
+[Binary Decision Diagrams](http://en.wikipedia.org/wiki/Binary_decision_diagram)
+in Clojure.
+
+
+## License
+
+Copyright © 2014 RedJack, LLC.
+
+Distributed under the Eclipse Public License either version 1.0 or (at your
+option) any later version.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-bdd "0.1.0-SNAPSHOT"
+  :description "Binary Decision Diagrams in Clojure"
+  :url "https://github.com/dcreager/clj-bdd"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
