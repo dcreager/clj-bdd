1dad4b8d1d621de02ff99cbc992c3ec7152ca2b0
Douglas Creager <douglas.creager@redjack.com>
2014-07-30 10:20:35 -0400

    BDD nodes
    The `Node` class represents an individual BDD node.  Pretty basic; it's
    just a record containing the variable, value, and low and high pointers.
    A leaf node has a value but not a low or high; an internal node has a
    low and a high but not a value.  A leaf node also has a special
    "variable" that is distinct from any other variable that you could
    provide for internal nodes.
    
    BDD nodes will be ordered by using the built-in `compare` function on
    the variable objects that the user passes in.  That lets you use
    numbers, symbols, whatever.  To provide a custom ordering, you'd have to
    create a new type with its own implementation of `compare`.  I'll
    probably add the ability to provide a compare function directly when
    creating a BDD to make that easier.
    
    A BDD maintains a cache of all of the nodes that have already been
    created, so that it's "reduced".  That means that you can compare nodes
    using `identical?` — if two nodes are not identical, then they represent
    different boolean formulas.

--- src/bdd/core.clj
+++ src/bdd/core.clj
@@ -1,1 +1,47 @@
 (ns bdd.core)
+
+
+;;-----------------------------------------------------------------------------
+;; BDD nodes
+
+(def ^:private terminal-variable (Object.))
+
+(defrecord Node [variable low high value])
+
+(defn- leaf-node? [node] (identical? (.variable node) terminal-variable))
+
+
+;;-----------------------------------------------------------------------------
+;; BDDs
+
+(defrecord BDD [nodes])
+
+(defn bdd [] (BDD. (atom {})))
+
+(defn- bdd-node
+  ([bdd variable low high value]
+   (let [k [variable low high value]]
+     (or
+       (@(.nodes bdd) k)
+       (let [node (Node. variable low high value)]
+         (swap! (.nodes bdd) assoc k node)
+         node)))))
+
+(defn- leaf
+  [bdd value] (bdd-node bdd terminal-variable nil nil value))
+
+(defn- internal
+  [bdd variable low high]
+  (if (identical? low high)
+    low
+    (bdd-node bdd variable low high nil)))
+
+
+;;-----------------------------------------------------------------------------
+;; Creating formulas
+
+(defn f [bdd] (leaf bdd false))
+(defn t [bdd] (leaf bdd true))
+
+(defn variable     [bdd variable] (internal bdd variable (f bdd) (t bdd)))
+(defn not-variable [bdd variable] (internal bdd variable (t bdd) (f bdd)))
--- test/bdd/core_test.clj
+++ test/bdd/core_test.clj
@@ -1,3 +1,26 @@
 (ns bdd.core-test
   (:require [clojure.test :refer :all]
             [bdd.core :refer :all]))
+
+
+(deftest test-bdd-nodes
+  (testing "bdd nodes"
+    (let [bdd (bdd)]
+
+      (is (= (:value (f bdd)) false))
+      (is (= (:value (t bdd)) true))
+
+      (is (identical? (f bdd) (f bdd)))
+      (is (identical? (t bdd) (t bdd)))
+      (is (not (identical? (f bdd) (t bdd))))
+
+      (is (identical? (:low (variable bdd :x)) (f bdd)))
+      (is (identical? (variable bdd :x) (variable bdd :x)))
+
+      (is (identical? (@#'bdd.core/internal bdd :a (f bdd) (f bdd))
+                      (f bdd)))
+      (is (identical? (@#'bdd.core/internal bdd :a (f bdd) (t bdd))
+                      (@#'bdd.core/internal bdd :a (f bdd) (t bdd))))
+    )
+  )
+)
